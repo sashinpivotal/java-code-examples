da7c67d6240983c9eb52f0375fbe582f0f26b8a6
Sang Shin <sangshinpassion@gmail.com>
2022-10-24 11:31:36 -0400

    change

--- src/main/java/_02_enum_examples/Enum_ComputerType.java
+++ src/main/java/_02_enum_examples/Enum_ComputerType.java
@@ -6,6 +6,7 @@ public class Enum_ComputerType {
     public static final int PC = 1;
     public static final int TABLET = 2;
     public static final int PHONE = 3;
+    public static final int TV = 4;
 
     public static void main(String[] args) {
 
@@ -20,11 +21,28 @@ public class Enum_ComputerType {
         // - Create a static method called
         //   "buyComputer(..)", which takes "ComputerType"
         //   enum type and quantity
-
-
-
+        buyComputerMethodUsingPublicStaticFinalConstants(2, 4);
+        buyComputerMethodUsingEnum(ComputerType.TABLET, 3);
     }
 
+    // use public static final
+    public static void buyComputerMethodUsingPublicStaticFinalConstants(int computerType, int quantity) {
+        System.out.println(computerType);
+    }
+
+    // use enum
+    public static void buyComputerMethodUsingEnum(ComputerType computerType, int quantity) {
+        System.out.println(computerType);
+    }
+
+
+}
+
+enum ComputerType {
+    PC,
+    TABLET,
+    PHONE,
+    TV
 }
 
 
