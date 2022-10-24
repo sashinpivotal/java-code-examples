976ce221971f6b79bd6bd3ebe74cea88a65db7c9
Sang Shin <sangshinpassion@gmail.com>
2022-10-24 14:41:51 -0400

    change

--- src/main/java/_04_array_examples/ArrayOfInt_Level2.java
+++ src/main/java/_04_array_examples/ArrayOfInt_Level2.java
@@ -1,18 +1,46 @@
 package _04_array_examples;
 
+import java.util.Arrays;
+
 public interface ArrayOfInt_Level2 {
     public static void main(String[] args) {
 
         // TODO: Exercise #1
         // - Let's say you have any array of int
         //   initialized with 3,9,6,2,1,10,5
-        // - Leverage the logic of the "findSmallestNumber(..)",
+        // - Leverage the logic of the "findSmallestNumberIndex(..)",
         //   which should return index of that smallestNumber
         // - See if you can use Integer.MAX_VALUE
         // - Create another array of int where
         //   the numbers are sorted, in other words,
         //   the new array should have 1,2,3,5,6,9,10
+        int[] unsortedInts = {3, 9, 6, 2, 1, 10, 5};
+        System.out.println(Arrays.toString(unsortedInts));
 
+        int[] sortedInts = new int[unsortedInts.length];
+        int currentIndex = 0;
+        for (int i = 0; i < unsortedInts.length; i++) {
+            int smallestNumberIndex = findSmallestNumberIndex(unsortedInts);
+            sortedInts[currentIndex++] = unsortedInts[smallestNumberIndex];
+            unsortedInts[smallestNumberIndex] = Integer.MAX_VALUE;
+        }
+
+        System.out.println(Arrays.toString(sortedInts));
+
+    }
+
+    static int findSmallestNumberIndex(int[] unsortedInts) {
+
+        int smallestNumber = unsortedInts[0];
+        int smallestNumberIndex = 0;
+
+        for (int i = 0; i < unsortedInts.length; i++) {
+            if (unsortedInts[i] < smallestNumber) {
+                smallestNumber = unsortedInts[i];
+                smallestNumberIndex = i;
+            }
+        }
+        return smallestNumberIndex;
     }
 
 }
