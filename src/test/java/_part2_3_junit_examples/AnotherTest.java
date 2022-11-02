3358a66641eb235a481d27a7bb83559ec3cac9e4
Sang Shin <sangshinpassion@gmail.com>
2022-11-02 14:31:29 -0400

    change

---
+++ src/test/java/_part2_3_junit_examples/AnotherTest.java
@@ -0,0 +1,6 @@
+package _part2_3_junit_examples;
+
+import junit.framework.TestCase;
+
+public class AnotherTest  {
+}
---
+++ src/test/java/mockito_examples/PersonTest.java
@@ -0,0 +1,10 @@
+package mockito_examples;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void getPerson() {
+    }
+}
