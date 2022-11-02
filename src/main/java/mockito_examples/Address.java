1448463b4f348de0b556dc054f9e62fed410d522
Sang Shin <sangshinpassion@gmail.com>
2022-11-02 17:14:10 -0400

    change

--- src/main/java/mockito_examples/Address.java
+++ src/main/java/mockito_examples/Address.java
@@ -5,11 +5,12 @@ public class Address {
     private String city;
     private String country;
 
-    public Address(String city) {
+    public Address(String city, String country) {
         this.city = city;
+        this.country = country;
     }
 
     public String getInfo() {
-        return city + " " + country;
+        return city + " " + country + "xx";
     }
 }
--- src/test/java/mockito_examples/PersonTest.java
+++ src/test/java/mockito_examples/PersonTest.java
@@ -13,11 +13,27 @@ public class PersonTest {
     @Mock
     private Address address;
 
+    // This test will fail due to a logic error in the Address dependency
     @Test
-    public void getPersonInfo_should_return_upperCased_info() {
+    public void getPersonInfo_should_return_upperCased_info_not_using_mock() {
 
         // arrange
-        BDDMockito.given(address.getInfo()).willReturn("boston in USA");
+        Person sang1 = new Person("sang");
+        Address boston = new Address("boston", "usa");
+        sang1.setAddress(boston);
+        String upperCasedPersonInfo = sang1.getUpperCasedPersonInfo();
+
+        // assert
+        Assert.assertEquals("SANG LIVES IN BOSTON USA", upperCasedPersonInfo);
+
+    }
+
+    // This test will succeed given that we mock the behavior of Address dependency
+    @Test
+    public void getPersonInfo_should_return_upperCased_info_using_mock() {
+
+        // arrange
+        BDDMockito.given(address.getInfo()).willReturn("boston USA");
 
         // act
         Person sang1 = new Person("sang");
@@ -25,10 +41,11 @@ public class PersonTest {
         String upperCasedPersonInfo = sang1.getUpperCasedPersonInfo();
 
         // assert
-        Assert.assertEquals("SANG LIVES IN BOSTON IN USA", upperCasedPersonInfo);
+        Assert.assertEquals("SANG LIVES IN BOSTON USA", upperCasedPersonInfo);
 
         // verify
         BDDMockito.verify(address).getInfo();
 
     }
+
 }
