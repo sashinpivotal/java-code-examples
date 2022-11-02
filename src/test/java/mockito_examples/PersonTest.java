062cbe56e8153302b8d80b877993534c9010c336
Sang Shin <sangshinpassion@gmail.com>
2022-11-02 17:30:19 -0400

    change

--- src/test/java/mockito_examples/PersonTest.java
+++ src/test/java/mockito_examples/PersonTest.java
@@ -17,6 +17,8 @@ public class PersonTest {
     // in the Address dependency
     // (In that respect, it is an integration testing
     // involving two classes not a unit test.)
+    // (Whether you want to use integration testing or
+    // unit testing or both is up to you.)
     @Test
     public void getPersonInfo_should_return_upperCased_info_not_using_mock() {
 
