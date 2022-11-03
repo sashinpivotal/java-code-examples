1474295a3aedb5bb9b35699bd28697cc3e7e9880
Sang Shin <sangshinpassion@gmail.com>
2022-11-03 08:39:28 -0400

    a new commit

--- src/main/java/_part2_4_generics/why_generics/Main.java
+++ src/main/java/_part2_4_generics/why_generics/Main.java
@@ -2,8 +2,6 @@ package _part2_4_generics.why_generics;
 
 public class Main {
 
-    // this is my own change in IntelliJ
-    // This is a main method
     public static void main(String[] args) {
 
         // Using non-generic class
