454cbb60a1e5e2ec89206fed56e2763c6a2cda1b
Sang Shin <sangshinpassion@gmail.com>
2022-11-03 07:32:58 -0400

    a new commit

--- src/main/java/_part2_4_generics/why_generics/Main.java
+++ src/main/java/_part2_4_generics/why_generics/Main.java
@@ -15,6 +15,5 @@ public class Main {
                 = new MyClassWithGenerics<>();
         MyClassWithGenerics<Customer> customerMyClassWithGenerics
                 = new MyClassWithGenerics<>();
-
     }
 }
