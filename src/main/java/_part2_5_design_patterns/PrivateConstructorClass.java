4c51064861bf1b679c18cd51dd2f1ee86e8548bb
Sang Shin <sangshinpassion@gmail.com>
2022-11-04 07:33:37 -0400

    change

---
+++ src/main/java/_part2_5_design_patterns/PrivateConstructorClass.java
@@ -0,0 +1,13 @@
+package _part2_5_design_patterns;
+
+public class PrivateConstructorClass {
+
+    private PrivateConstructorClass() {
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // new PrivateConstructorClass();
+    }
+}
